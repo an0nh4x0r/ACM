d6074e8b57639e0e99a6eeaea849b5d61dc09535
Swapnil Singh <swappp7singh@gmail.com>
2017-02-23 02:10:07 +0530

    Basic angular app ready for working with ACM website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { EventsComponent } from './events/events.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { BlogComponent } from './blog/blog.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    JumbotronComponent,
+    FooterComponent,
+    AboutComponent,
+    EventsComponent,
+    GalleryComponent,
+    BlogComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: '', component: JumbotronComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'events', component: EventsComponent },
+      { path: 'gallery', component: GalleryComponent },
+      { path: 'blog', component: BlogComponent },
+      { path: 'register', component: RegisterComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
